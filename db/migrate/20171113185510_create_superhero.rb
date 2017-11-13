6da6fa64734a534da86c6d55bd78d0b17760b651
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-13 19:01:02 +0000

    automatically backed up by learn

--- db/migrate/20171113185510_create_superhero.rb
+++ db/migrate/20171113185510_create_superhero.rb
@@ -6,7 +6,7 @@ class CreateSuperhero < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :power
       t.text :bio
-      t.team_id :integer
+      t.integer :team_id
     end
 
   end
