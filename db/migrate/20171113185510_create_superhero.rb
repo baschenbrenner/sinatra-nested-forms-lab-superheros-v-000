84429523ba2658565a4b5b2ef67e596de12d16e5
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-13 18:59:34 +0000

    automatically backed up by learn

--- db/migrate/20171113185510_create_superhero.rb
+++ db/migrate/20171113185510_create_superhero.rb
@@ -2,7 +2,7 @@ class CreateSuperhero < ActiveRecord::Migration[5.1]
 
 
   def change
-    create_table :super_hero do |t|
+    create_table :superheroes do |t|
       t.string :name
       t.string :power
       t.string :bio
